c20bc9c07fec00900f50f204d1e367898fe7c2ad
Steven Rosa <s.rosa21@icloud.com>
2022-01-10 19:08:03 -0500

    Added some server enpoints successfully

--- api/server.js
+++ api/server.js
@@ -1,3 +1,38 @@
-// BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express();
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json())
+
+server.get('/api/users', (req,res) => {
+    User.find()
+        .then(resp => {
+            res.status(200).json(resp)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+
+})
+
+server.post('/api/users', (req,res) => {
+
+})
+
+server.get('/api/users/:id', (req,res) => {
+    User.findById(req.params.id)
+        .then(resp =>{
+            res.status(200).json(resp)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+server.delete('/api/users/:id', (req,res) => {
+
+})
+
+server.put('/api/users/:id', (req,res) => {
+
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
